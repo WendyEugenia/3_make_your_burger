feb915bb538f26e68242b9987f1f9f2feae436f3
WendyEugenia <wendy.t.n@hotmail.com>
2022-11-03 12:28:28 -0300

    Projeto.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    
+    <p>Footer ggggg</p>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+    
+
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+
+</script>
+
+<style scoped>
+
+</style>
